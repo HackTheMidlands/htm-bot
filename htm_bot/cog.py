c6d58aa554bb1915fee80fd2c46ea521744a36ef
Charles de Freitas <me@chasbob.dev>
2021-10-28 21:03:14 +0100

    Add CTF flags command

--- htm_bot/cog.py
+++ htm_bot/cog.py
@@ -8,6 +8,9 @@ from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Bot, Cog
 from discord_slash import SlashContext, cog_ext
+from discord_slash.utils.manage_commands import create_option
+from discord_slash.context import MenuContext
+from discord_slash.model import ContextMenuType
 
 
 class PingPongCog(Cog):
@@ -30,6 +33,41 @@ class SharkCog(Cog):
         self.typerCtx = typer_ctx
         self.url = typer_ctx.obj["api_url"]
         self.token = typer_ctx.obj["api_token"]
+        self.flags = typer_ctx.obj["flags"]
+
+    @cog_ext.cog_slash(
+        name="flag",
+        guild_ids=[724630949521784852],
+        options=[
+            create_option(
+                name="challenge",
+                description="Challenge.",
+                option_type=3,
+                required=False,
+            ),
+            create_option(
+                name="flag", description="Flag.", option_type=3, required=False
+            ),
+        ],
+    )
+    async def _ctf_flag(self, ctx: SlashContext, challenge: str, flag: str):
+        if correct := self.flags.get(challenge):
+            if correct == flag:
+                await self._submit(ctx, challenge, ctx.author_id)
+            else:
+                await ctx.send(content="Nice try!")
+        else:
+            await ctx.send(content="I don't recognise that flag...")
+
+    async def _submit(self, ctx, name, user):
+        try:
+            resp = requests.post(f"{self.url}/{name}/{user}?token={self.token}")
+            typer.echo(resp)
+        except Exception as e:
+            typer.echo(e, err=True)
+            await ctx.send("Oops! Something went wrong.")
+            return
+        await ctx.send("Successfully submitted {name}!")
 
     @cog_ext.cog_slash(name="shark", guild_ids=[724630949521784852])
     async def _shark_tank(self, ctx: SlashContext):
--- htm_bot/main.py
+++ htm_bot/main.py
@@ -4,6 +4,8 @@
 import re
 
 import typer
+from pathlib import Path
+import json
 
 from .bot import bot
 from .cog import setup
@@ -19,8 +21,15 @@ def run(
         default="https://achivements.hackthemidlands.com/api", envvar="API_BASE"
     ),
     api_token: str = typer.Argument(default="", envvar="API_TOKEN"),
+    ctf_flags: Path = typer.Argument(default=None, envvar="CTF_FLAGS_FILE"),
 ):
     ctx.obj = {"api_url": api_url, "api_token": api_token}
+    if ctf_flags:
+        with open(ctf_flags, "r") as file:
+            ctx.obj["flags"] = json.load(file)
+    else:
+        ctx.obj["flags"] = {}
+    typer.echo(ctx.obj)
     typer.echo(f'Using API endpoint: {api_url}, token: {re.sub(".", "*", api_token)}')
     setup(bot, ctx)
     bot.run(token)
